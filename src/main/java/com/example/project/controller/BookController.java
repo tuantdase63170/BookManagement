0a4cda3aa1f53859e74733ddf2fd91aa06f7b7c8
tuantda8736 <tuan.tda.8736.outlook@gmail.com>
2021-07-08 10:14:19 +0700

    Add @CrossOrigin for controllers

--- src/main/java/com/example/project/controller/BookController.java
+++ src/main/java/com/example/project/controller/BookController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.example.project.model.BookDTO;
 import com.example.project.service.BookService;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/api/book")
 public class BookController {
